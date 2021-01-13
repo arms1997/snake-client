7838c20d264ca208a7bc4216ef1f096a198cd448
arms1997 <armeen.hadizadeh@gmail.com>
2021-01-13 18:18:03 +0000

    refactored the code to modularize the input elements

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 const {connect} = require('./client')
+const {setupInput} = require('./input')
 
-console.log('Connecting ...')
-
-const setupInput = function() {
-  const stdin = process.stdin;
-
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-
-  stdin.on('data', handleUserInput)
-
-  return stdin
-}
-
-const handleUserInput = (data) => {
-  console.log(data)
-
-  if(data === '\u0003'){
-    process.exit()
-  }
-}
-setupInput()
-
+console.log('Connecting...')
 connect()
+
+setupInput()
